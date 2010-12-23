aca756b182f52a5eb169e91a077c38273a46bd55
mb.netmb <mb.netmb@934d959c-1f36-3724-41df-31ef3166a9db>
2010-12-23 19:27:00 +0000

    Bug fixing. Some command translation

--- Software/eBusMonitor-Perl/eBusMonitor.pl
+++ Software/eBusMonitor-Perl/eBusMonitor.pl
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 use POE;
 use EBusFilter;
+use EBus;
 use POE::Component::Client::TCP;
 
 my %names;
@@ -47,24 +48,70 @@ exit 0;
 
 sub dataIn {
 	my ( $kernel, $heap, $data ) = @_[ KERNEL, HEAP, ARG0 ];
-	print "QQ:" . $data->{QQ};
-	print " ZZ:" . $data->{ZZ};
-	print " PB:" . $data->{PB};
-	print " SB:" . $data->{SB};
-	print " NN:" . $data->{NN};
-	for ( my $i = 0 ; $i < $data->{NN} ; $i++ ) {
-		print " DA$i:" . $data->{DA}[$i];
+	my $ebus = EBus->new();
+
+	# Sollwertuebertragung des Reglers an andere Regler
+	if ( $data->{PB} == 8 && $data->{SB} == 0 ) {
+		print
+"# $data->{QQ} $data->{ZZ} ############## Sollwertuebertragung des Reglers an andere Regler ###################\n";
+		print "Kesselsoll:"
+		  . $ebus->data2b( $data->{DA}[0], $data->{DA}[1] ) . "\n";
+		print "Aussentemp.:"
+		  . $ebus->data2b( $data->{DA}[2], $data->{DA}[3] ) . "\n";
+		print "Brauchwasserbereitung aktiv\n" if ( $data->{DA}[5] == 0 );
+		print "Heizkreis aktiv\n"             if ( $data->{DA}[5] == 1 );
+		print "Brauchwassersoll:"
+		  . $ebus->data2b( $data->{DA}[6], $data->{DA}[7] ) . "\n";
+
 	}
-	print " CHK:" . $data->{CHK};
-	print " ACK:" . $data->{ACK};
-	if ( $data->{SNN} ) {
-		print " SNN:" . $data->{SNN};
-		for ( my $i = 0 ; $i < $data->{SNN} ; $i++ ) {
-			print " SDA$i:" . $data->{SDA}[$i];
+
+	# Betriebsdaten des Reglers an den Feuerungsautomaten
+	elsif ( $data->{PB} == 5 && $data->{SB} == 7 ) {
+		print
+"# $data->{QQ} $data->{ZZ} ############## Betriebsdaten des Reglers an den Feuerungsautomaten ###################\n";
+		print "Brenner abschalten\n"      if ( $data->{DA}[0] == 0x00 );
+		print "Brauchwassser bereitung\n" if ( $data->{DA}[0] == 0x55 );
+		print "Heizbetrieb\n"             if ( $data->{DA}[0] == 0xAA );
+		print "Kesselsollwert"
+		  . $ebus->data2c( $data->{DA}[2], $data->{DA}[3] ) . "\n";
+		print "Kesselsollwert-Druck"
+		  . $ebus->data2b( $data->{DA}[4], $data->{DA}[5] ) . "\n";
+		print "Stellgrad:" . $ebus->data1c( $data->{DA}[6] ) . "\n";
+		print "Brauchwassersoll:" . $ebus->data1c( $data->{DA}[7] ) . "\n";
+
+	}
+
+	# Betriebsdaten des Feuerungsautomaten an den Regler - Block 1
+	elsif ( $data->{PB} == 5 && $data->{SB} == 3 && $data->{DA}[0] == 1 && $data->{QQ} == 3 && $data->{ZZ} == 254) {
+		print
+"# $data->{QQ} $data->{ZZ} ############## Betriebsdaten des Feuerungsautomaten an den Regler - Block 1  ###################\n";
+		print "Stellgrad Kesselleistung" . $data->{DA}[3] . "\n";
+		print "Kesseltemp." . $ebus->data1c( $data->{DA}[4] ) . "\n";
+		print "RŸcklauftemp.:" . $data->{DA}[5] . "\n";
+		print "Boilertemp.:" . $data->{DA}[6] . "\n";
+		print "Aussentemp.:" . $data->{DA}[7] . "\n";
+	}
+	else {
+		print "QQ:" . $data->{QQ};
+		print " ZZ:" . $data->{ZZ};
+		print " PB:" . $data->{PB};
+		print " SB:" . $data->{SB};
+		print " NN:" . $data->{NN};
+		for ( my $i = 0 ; $i < $data->{NN} ; $i++ ) {
+			print " DA$i:" . $data->{DA}[$i];
 		}
-		print " SCHK:" . $data->{SCHK};
+		print " CHK:" . $data->{CHK};
+		print " ACK:" . $data->{ACK};
+		if ( $data->{SNN} ) {
+			print " SNN:" . $data->{SNN};
+			for ( my $i = 0 ; $i < $data->{SNN} ; $i++ ) {
+				print " SDA$i:" . $data->{SDA}[$i];
+			}
+			print " SCHK:" . $data->{SCHK};
+		}
+		print " (!)"  if ( $data->{CHKSUMFALSE} );
+		print " (!!)" if ( $data->{SCHKSUMFALSE} );
+		print "\n";
 	}
-	print " (!)"  if ( $data->{CHKSUMFALSE} );
-	print " (!!)" if ( $data->{SCHKSUMFALSE} );
-	print "\n";
+
 }
